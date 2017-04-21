3ad298074a9ea0a7c5cd515c5e4ab2e64170fcb2
yuzhiheng990621 <907504836@qq.com>
2017-04-21 13:37:23 +0800

    二十一游戏

--- DaLao_DaYu/main.c
+++ DaLao_DaYu/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-void main()
+int main(void)
 {
-    for(int i=1;i<=100;i++){
-        for(int j=1;j<=100-i;j++){
-            if((100-i-j)%3==0 && (5*i+3*j+1*(100-i-j)/3)==100 )
-            printf("长空%d天  志乐%d天  不语%d天\n",i,j,100-i-j);
-        }}}
+    for(int n,i=1;i<=7;i++)
+    {
+        scanf("%d",&n);
+        printf("%d\n",3*i);
+    }
+}
