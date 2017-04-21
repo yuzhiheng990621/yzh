c61736b0cdd8d8311b3f44cde41f53844a204278
yuzhiheng990621 <907504836@qq.com>
2017-04-21 14:16:14 +0800

    简化

--- DaLao_DaYu/main.c
+++ DaLao_DaYu/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 int main(void)
 {
-    for(int n,i=1;i<=7;i++)
+    srand(time(NULL));
+    (rand()%2+1==1)?printf("抢二十一游戏\n我先输入\n%d\n",rand()%2+1):printf("抢二十一游戏\n你先输入\n");
+        for(int n,i=1;i<=7;i++)
     {
         scanf("%d",&n);
-        printf("%d\n",3*i);
+        3*i>n?printf("%d\n",3*i):printf("%d\n",n+rand()%2+1);
     }
 }
