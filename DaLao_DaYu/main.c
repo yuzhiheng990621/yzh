745193d11bbfabbf81287e1ab1a0c759f2ed3e16
yuzhiheng990621 <907504836@qq.com>
2017-04-21 14:40:56 +0800

    极简

--- DaLao_DaYu/main.c
+++ DaLao_DaYu/main.c
@@ -8,6 +8,6 @@ int main(void)
         for(int n,i=1;i<=7;i++)
     {
         scanf("%d",&n);
-        3*i>n?printf("%d\n",3*i):printf("%d\n",n+rand()%2+1);
+        (3*i-n>=3 || n-i*3>=3)?printf("你违反了游戏规则！！！"):3*i>n?printf("%d\n",3*i):printf("%d\n",n+rand()%2+1);
     }
 }
